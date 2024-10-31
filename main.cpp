0d65075768c65c1e61362949eab8dd3976aa7e7d
TomasBrigido <tomas.brigido@mi.unc.edu.ar>
2024-10-31 16:26:47 -0300

    tryValues and solveSudoku

--- main.cpp
+++ main.cpp
@@ -9,19 +9,25 @@
 using namespace std;
 
 /*
-*  @brief   Define the Grid type as vector of vectors
+*  @brief   Defino el tipo Grid como un vector de vectores de optional int.
 *  @details Uso Grid -> Alias para el tipo de matriz que necesito.
 *           El dato optional<int> es similar a maybe en haskell.
 *  
 *  @param   vector<vector<optional<int>>>
 */
 using Grid = vector<vector<optional<int>>>;
+
+/*
+*  @brief   Defino el arreglo de numeros del 1 al 9.
+*  
+*  @param   [1.2.3.4.5.6.7.8.9]
+*/
 const int numeros[9] = {1,2,3,4,5,6,7,8,9};
-optional <Grid> tryValues(const Grid grid, optional<pair<int, int>> coords, const int numeros[9], int num = 0);
+optional <Grid> tryValues(const Grid grid, pair<int, int> coords, const int numeros[9], int num = 0);
 
 
 /*
-*  @brief   Example Sudoku grid with some initial values
+*  @brief   Grilla inicial del sudoku.
 */
 const Grid grid = {
       {5,nullopt,1,nullopt,nullopt,nullopt,nullopt,nullopt,nullopt},
@@ -36,7 +42,7 @@ const Grid grid = {
    };
 
 /*
-*  @brief   Pretty-print the Sudoku grid
+*  @brief   Imepresion de la grilla.
 */
 void printGrid(Grid grid){
    for_each(grid.begin(), grid.end(),[](vector<optional<int>> line){
@@ -51,9 +57,9 @@ void printGrid(Grid grid){
 }
 
 /*
-*  @brief   Set a value at a specific row and column in the Grid
+*  @brief   Defino un valor en un lugar dado de la grilla.
 *
-*  @return  A new Grid
+*  @return  Una nueva grilla con el valor definido.
 */
 Grid setValue(Grid grid, int row, int col, optional<int> value){
    grid[row][col] = value;
@@ -61,38 +67,38 @@ Grid setValue(Grid grid, int row, int col, optional<int> value){
 }
 
 /*
-*  @brief   Check if a value is valid in the row
-*  @details Funcion find busca el valor dado. Si no lo encuentra devueve grid.end()
+*  @brief   Verifico si el valor dado es valido en la fila.
+*  @details La funcion find busca el valor dado. Si no lo encuentra devueve grid.end()
 *
-*  @return  valid     -> true;
-*  @return  not valid -> false;
+*  @return  True o false dependiendo si el valor es valido o no.
 */
-bool validInRow (Grid grid, int row, int value){
+bool validInRow (const Grid& grid, int row, int value){
    return find(grid[row].begin(), grid[row].end(),value) == grid[row].end();
 }
 
 /*
-*  @brief   Check if a value is valid in the col
+*  @brief   Verifico si el valor dado es valido en la columna.
 *  @details Funcion none_of() recorre cada fila y en el valor de la columna compara con value.
 *           Retorna true si ningun elemento cumple la funcion lambda y false si al menos un
-*           elemento lo cumple
+*           elemento lo cumple.
 *
-*  @return  valid     -> true;
-*  @return  not valid -> false;
+*  @return  True o false dependiendo si el valor es valido o no.
 */
-bool validInCol(Grid grid, int col, int value){
-   return none_of(grid.begin(), grid.end(), [col, value](const vector<optional<int>> row) { return row[col] == value; });
+bool validInCol(const Grid& grid, int col, int value){
+   return none_of(grid.begin(), grid.end(), [col, value](const vector<optional<int>> row) {
+      return row[col] == value; 
+   });
 }
 
 /*
-*  @brief   Check if a value is valid in the subGrid (3x3)
-*  @details Primero defino los limites de la subgrid.
+*  @brief   Verifico si el valor el valido en la sub grilla 3x3.
+*  @details Primero defino los limites de la sub grilla.
 *           Luego la recorro con el for_each() guardando los valores en subgrid.
 *           Y con el find() controlo si el dato n esta en la subgrid.
 *
-*  @return  valid -> true;
+*  @return  True o false dependiendo si el valor es valido o no.
 */
-bool validInSubgrid (Grid grid, int row, int col, int n){
+bool validInSubgrid (Grid& grid, int row, int col, int n){
 
    const int subgridRowStart = ((((row +1) -1)/3)*3);
    const int subgridColStart = ((((col +1) -1)/3)*3);
@@ -101,7 +107,7 @@ bool validInSubgrid (Grid grid, int row, int col, int n){
    const auto lineFinish = grid.begin() + subgridRowStart + 3;
 
    
-   vector <int>  subgrid;
+   vector <optional<int>>  subgrid;
 
 
    for_each(lineStart, lineFinish,[&subgrid,subgridColStart](vector<optional<int>> &line){
@@ -110,7 +116,7 @@ bool validInSubgrid (Grid grid, int row, int col, int n){
       const auto colFinish = line.begin() + subgridColStart + 3;
    
       for_each(colStart,colFinish,[&subgrid](const optional<int> &x){
-            if(x == nullopt){
+         if(x == nullopt){
             
          }else{
             subgrid.push_back(x.value());
@@ -122,30 +128,26 @@ bool validInSubgrid (Grid grid, int row, int col, int n){
 }
 
 /*
-*  @brief   Check if a value is valid in row col and subgrid
+*  @brief   Verifico que el valor sea valido en la fila, la columna y la sub grilla
 *
-*  @return  valid -> true;
+*  @return  True o false dependiendo si el valor es valido o no.
 */
-bool isValid(Grid grid,int row, int col, int n){
-
-   if(validInRow(grid,row,n) && validInCol(grid,col,n) && validInSubgrid(grid,row,col,n)){
-      return true;
-   }
-   return false;
+bool isValid(Grid& grid,int row, int col, int n){
+   return validInRow(grid,row,n) && validInCol(grid,col,n) && validInSubgrid(grid,row,col,n);
 }
 
 /*
-*  @brief   Find the coords of an empy cell
+*  @brief   Encuentra las cordenadas de una celda vacia.
 *  @details Utilizo una funcion recursiva para ir recorriendo linea por linea la matriz.
 *           Utilizo find_if() para entonctrar la celda que no tiene valor.
 *           Devuelvo el par con las cordenadas de la celda vacia.
 *
-*  @return  Pair of an empty cell coords;
+*  @return  Par que indica la cordenanada de la celda vacia.
 */
-optional<pair<int, int>> findEmptyCell(const Grid grid, int row = 0) {
+optional<pair<int, int>> findEmptyCell(const Grid& grid, int row = 0) {
 
    //Condicion de corte de la recusividad
-   if (row >= grid.size()) {
+   if (row >= (int)(grid.size())) {
       return nullopt;
    }
 
@@ -162,44 +164,42 @@ optional<pair<int, int>> findEmptyCell(const Grid grid, int row = 0) {
 }
 
 /*
-*  @brief   Find the coords of an empy cell
-*  @details Utilizo una funcion recursiva para ir recorriendo linea por linea la matriz.
-*           Utilizo find_if() para entonctrar la celda que no tiene valor.
-*           Devuelvo el par con las cordenadas de la celda vacia.
+*  @brief   Resuelve la grilla del sudoku.
+*  @details Busca celdas vacias de la grilla y prueba valores del 1 al 9.
+*           Si no encuentra celdas vacias es porque ya esta resuelta la grilla.
 *
-*  @return  Pair of an empty cell coords;
+*  @return  La grilla con el sudoku resuelto.
 */
-optional <Grid> solveSudoku (const Grid grid){
-   if(findEmptyCell(grid) == nullopt){
-      return grid; //Solucion del sudoku
-   }else{
-      tryValues(grid,findEmptyCell(grid),numeros);
+optional <Grid> solveSudoku (const Grid& grid){
+
+   auto emptyCell = findEmptyCell(grid);
+   if(emptyCell == nullopt){
+        return grid; //Solucion del sudoku
    }
+    auto [row, col] = *emptyCell;
+   return tryValues(grid,make_pair(row, col), numeros);
+   
 }
+
 /*
-*  @brief   Find the coords of an empy cell
-*  @details Utilizo una funcion recursiva para ir recorriendo linea por linea la matriz.
-*           Utilizo find_if() para entonctrar la celda que no tiene valor.
-*           Devuelvo el par con las cordenadas de la celda vacia.
+*  @brief   Prueba valores en las celdas vacias.
+*  @details Valida que el valor sea valido y si es asi, defino ese valor en la celda.
 *
-*  @return  Pair of an empty cell coords;
+*  @return  Grilla con los valores definidos.
 */
+optional <Grid> tryValues(Grid grid, pair<int, int> coords, const int numeros[9], int num){
 
-optional <Grid> tryValues(const Grid grid, optional<pair<int, int>> coords, const int numeros[9], int num){
+   if (num >= 9) return nullopt;//Condicion para cunado se acaben los numeros.
    
-   if(isValid(grid,coords->first,coords->second,numeros[num])){
-
-      const optional <Grid> solution = solveSudoku(setValue(grid,coords->first,coords->second,numeros[0]));
-
-      if(solution != nullopt){
-         return solution;
-      }else{
-         tryValues(grid,coords,numeros,num+1);
+   if(isValid(grid,coords.first,coords.second,numeros[num])){
+      Grid newGrid = setValue(grid, coords.first, coords.second, numeros[num]); 
+      auto solution = solveSudoku(newGrid);
+      if(solution){
+            return solution;
       }
-   }else{
-      tryValues(grid,coords,numeros,num+1);
    }
-   
+
+   return tryValues(grid, coords, numeros, num+1);
 }
 
 int main() {
@@ -209,11 +209,14 @@ int main() {
    cout << "\nSolving...\n\n";
 
    const optional <Grid> solution = solveSudoku(grid);
+
    if(solution != nullopt){
       cout<<"Solved Sudoku grid: \n\n";
       printGrid(solution.value());
+      cout<<"\n";
+   }else{
       cout<<"No solution found. \n\n";
-
    }
+
    return 0;
 }
